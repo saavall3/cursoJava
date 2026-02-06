d27c4761b8685bc43e10e1939ca3820b94b99f63
Use <Use@192.168.1.207>
2026-02-06 14:05:56 +0100

    Creacion del proyecto

---
+++ src/clases/CafeteraException.java
@@ -0,0 +1,13 @@
+package clases;
+
+public class CafeteraException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CafeteraException(String mensaje) {
+		super(mensaje);
+	}
+}
